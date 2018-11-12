1368790e512aa833ee837f494210c1ea660d2f64
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-11-12 07:02:23 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,8 @@ def key_for_min_value(name_hash)
     
     name_hash.each do |key, value|
       if lowest_value > value
-        value = lowest_value
+         lowest_value = value
+         lowest_name = key
       end
     end
     name_hash
